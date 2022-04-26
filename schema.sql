3f5e237235819e25682e30979179be6209d87901
cameronmitchell18 <CameronMitchell18@outlook.com>
2022-04-26 12:12:32 -0400

    List of employees ready to retire

--- schema.sql
+++ schema.sql
@@ -97,8 +97,26 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	
 	to_date DATE NOT NULL, 
-	
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	
+		
 	PRIMARY KEY (emp_no)
 );
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
