f9391607cac76dd45420ab2573be93c2f9deb482
cameronmitchell18 <CameronMitchell18@outlook.com>
2022-04-26 20:18:18 -0400

    new db

--- queries.sql
+++ queries.sql
@@ -61,8 +61,9 @@ ON departments.dept_no = dept_manager.dept_no;
 -- Joining retirement_info and dept_emp tables
 SELECT retirement_info.emp_no,
     retirement_info.first_name,
-retirement_info.last_name,
-    dept_employees.to_date
+    retirement_info.last_name,
+dept_employees.to_date
+INTO current_emp
 FROM retirement_info
 LEFT JOIN dept_employees
 ON retirement_info.emp_no = dept_employees.emp_no;
